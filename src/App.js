3ce8a95ca8715e72a7034efd4c820813cbfbd2fc
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-05-24 13:29:38 +0530

    Usage of useCallback

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 
-import React,{Suspense, lazy} from "react"
+import React, {useState,memo,useCallback} from "react"
 import './App.css';
-const Users =lazy(()=>import("./Components/Users"))
-const Posts =lazy(()=>import("./Components/Posts"))
 
 function App() {
+
+  const [input,setInput]=useState("")
+  const [count,setCount]=useState(0);
+
+  const IncrementCount= useCallback(()=>{
+    setCount(count+1);
+  },[count])
+
   return (
     <div className="App">
-      <Suspense fallback={<h3>Loading...</h3>}>
-        <Users/>
-        <Posts/>
-      </Suspense>
+       <input
+          type='text'
+          value={input}
+          onChange={(e)=>setInput(e.target.value)}
+       />
+       <button onClick={()=>setCount(count+1)}>Increment Count</button>
+       <h3>Text input is {input}</h3>
+       <h3>Count is {count}</h3>
+       <hr/>
+       <ChildComponent IncrementCount={IncrementCount} count={count}/>
     </div>
-  )
+  );
 }
+
+const ChildComponent=memo(function ChildComponent({count,IncrementCount}){
+  console.log("Rendering child components");
+  return(
+
+    <>
+      <button onClick={IncrementCount}>Click ME</button>
+      <h4>Count from Child component is {count}</h4>
+    </>
+  ) 
+})
 
 
 
