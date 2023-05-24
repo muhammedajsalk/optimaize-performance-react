e91a7d91d09de17ab5c07540ba9abe785b7fff1e
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-05-24 12:49:30 +0530

    Introduction to memization(not started sample

--- src/App.js
+++ src/App.js
@@ -4,33 +4,29 @@ import './App.css';
 
 function App() {
 
-  
+  const [input,setInput]=useState("")
+  const [count,setCount]=useState(0);
 
   return (
     <div className="App">
-       <FormInput/>
-       <ChildComponent/>
-    </div>
-  );
-}
-
-function FormInput(){
-  const [input,setInput]=useState("")
-  return(
-    <>
-      <input
+       <input
           type='text'
           value={input}
           onChange={(e)=>setInput(e.target.value)}
        />
+       <button onClick={()=>setCount(count+1)}>Increment Count</button>
        <h3>Text input is {input}</h3>
-    </>
-  )
+       <h3>Count is {count}</h3>
+       <hr/>
+       <ChildComponent count={count}/>
+    </div>
+  );
 }
 
-function ChildComponent(){
+
+function ChildComponent({count}){
   console.log("Rendering child components");
-  return <h4>Child component</h4>
+  return <h4>Count from Child component is {count}</h4>
 }
 
 export default App;
