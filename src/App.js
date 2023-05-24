2da0de9dfdbd0f2dd1b5b538d796c6b37dc3bef5
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-05-24 20:07:49 +0530

    Lazy loading for Images

--- src/App.js
+++ src/App.js
@@ -1,50 +1,34 @@
 
-import React, {useState,memo,useMemo,useCallback} from "react"
+import React from "react"
 import './App.css';
-const myFunction =(count)=>{
-  for (let i=0;i<100000;i++){}
-  return count * 3;
-}
+import LazyLoad from 'react-lazy-load';
 
-function App() {
+const styles={
+  textAlign:"center"
+};
 
-  const [input,setInput]=useState("")
-  const [count,setCount]=useState(0);
-
-  const IncrementCount= useCallback(()=>{
-    setCount(count+1);
-  },[count])
-
-  const myCount=useMemo(()=>{
-    return myFunction(count);
-  },[count])
-
-  return (
-    <div className="App">
-       <input
-          type='text'
-          value={input}
-          onChange={(e)=>setInput(e.target.value)}
-       />
-       <button onClick={()=>setCount(count+1)}>Increment Count</button>
-       <h3>Text input is {input}</h3>
-       <h3>Count is {myCount}</h3>
-       <hr/>
-       <ChildComponent IncrementCount={IncrementCount} count={count}/>
-    </div>
-  );
-}
-
-const ChildComponent=memo(function ChildComponent({count,IncrementCount}){
-  console.log("Rendering child components");
-  return(
-
-    <>
-      <button onClick={IncrementCount}>Click ME</button>
-      <h4>Count from Child component is {count}</h4>
-    </>
-  ) 
-})
+const App =()=>(
+  <div style={styles} >
+     <LazyLoad height={762}>
+      <img src="https://placedog.net/500" />
+    </LazyLoad>
+    <LazyLoad height={762}>
+      <img src="https://placedog.net/501" />
+    </LazyLoad>
+    <LazyLoad height={762}>
+      <img src="https://placedog.net/502" />
+    </LazyLoad>
+    <LazyLoad height={762}>
+      <img src="https://placedog.net/503" />
+    </LazyLoad>
+    <LazyLoad height={762}>
+      <img src="https://placedog.net/504" />
+    </LazyLoad>
+    <LazyLoad height={762}>
+      <img src="https://placedog.net/505" />
+    </LazyLoad>
+  </div>
+);
 
 
 
